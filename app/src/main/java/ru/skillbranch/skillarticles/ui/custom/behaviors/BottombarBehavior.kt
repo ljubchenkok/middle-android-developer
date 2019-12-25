144b56cd5d3590ad059d25c64ef6dac35559cc20
Konstantin Ljubchenko <ljubchenko.k@gmail.com>
2019-12-25 16:51:34 +0300

    added BottombarBehavior

---
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/BottombarBehavior.kt
@@ -0,0 +1,27 @@
+package ru.skillbranch.skillarticles.ui.custom.behaviors
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.ViewCompat
+
+class BottombarBehavior<V : View>(context: Context, attrs: AttributeSet) :
+    CoordinatorLayout.Behavior<V>(context, attrs) {
+
+    override fun onStartNestedScroll(
+        coordinatorLayout: CoordinatorLayout, child: V, directTargetChild: View, target: View, axes: Int, type: Int
+    ): Boolean {
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL
+    }
+
+    override fun onNestedPreScroll(
+        coordinatorLayout: CoordinatorLayout, child: V, target: View, dx: Int, dy: Int, consumed: IntArray, type: Int
+    ) {
+        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
+        val childHeight = child.height.toFloat()
+        val childTranslation = child.translationY + dy
+        val translation = if (childHeight < childTranslation) childHeight else childTranslation
+        child.translationY = if(translation > 0) translation else 0f
+    }
+}

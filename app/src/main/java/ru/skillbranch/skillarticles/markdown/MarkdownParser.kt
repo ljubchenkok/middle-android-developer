6b9f98d96cceaa7daee9f098e71b1679bfb8d01b
Konstantin Ljubchenko <ljubchenko.k@gmail.com>
2020-02-19 13:03:32 +0300

    added BlockCode Regexp

--- app/src/main/java/ru/skillbranch/skillarticles/markdown/MarkdownParser.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/markdown/MarkdownParser.kt
@@ -17,12 +17,12 @@ object MarkdownParser {
     private const val RULE_GROUP = "(^[-*_]{3}$)"
     private const val INLINE_GROUP = "((?<!`)`[^`\\s].*?[^`\\s]?`(?!`))"
     private const val LINK_GROUP = "(\\[[^\\[\\]]*?]\\(.+?\\)|^\\[*?]\\(.*?\\))"
-//    private const val BLOCK_CODE_GROUP = "" //TODO implement me
+    private const val BLOCK_CODE_GROUP = "(\\n```[\\n\\s\\S]*?```\\n)"
 //    private const val ORDER_LIST_GROUP = "" //TODO implement me
 
     //result regex
     private const val MARKDOWN_GROUPS = "$UNORDERED_LIST_ITEM_GROUP|$HEADER_GROUP|$QUOTE_GROUP" +
-            "|$ITALIC_GROUP|$BOLD_GROUP|$STRIKE_GROUP|$RULE_GROUP|$INLINE_GROUP|$LINK_GROUP"
+            "|$ITALIC_GROUP|$BOLD_GROUP|$STRIKE_GROUP|$RULE_GROUP|$INLINE_GROUP|$LINK_GROUP|$BLOCK_CODE_GROUP"
 //    private const val MARKDOWN_GROUPS = "$UNORDERED_LIST_ITEM_GROUP|$HEADER_GROUP|$QUOTE_GROUP" +
 //            "|$ITALIC_GROUP|$BOLD_GROUP|$STRIKE_GROUP|$RULE_GROUP|$INLINE_GROUP|$LINK_GROUP"
 //             |$BLOCK_CODE_GROUP|$ORDER_LIST_GROUP optionally
@@ -85,6 +85,7 @@ object MarkdownParser {
         var lastStartIndex = 0
 
         loop@ while (matcher.find(lastStartIndex)) {
+            val substring = string.substring(lastStartIndex)
             val startIndex = matcher.start()
             val endIndex = matcher.end()
             if (lastStartIndex < startIndex) {
@@ -92,7 +93,7 @@ object MarkdownParser {
             }
             var text: CharSequence
             //groups range for iterate by groups (1..9) or (1..11) optionally
-            val groups = 1..9
+            val groups = 1..10
             var group = -1
             for (gr in groups) {
                 if (matcher.group(gr) != null) {
@@ -182,10 +183,14 @@ object MarkdownParser {
                     parents.add(element)
                     lastStartIndex = endIndex
                 }
-//                //10 -> BLOCK CODE - optionally
-//                10 -> {
-//                    //TODO implement me
-//                }
+                //10 -> BLOCK CODE - optionally
+                10 -> {
+                    text = string.subSequence(startIndex.plus(4), endIndex.minus(4))
+                    val subs = findElements(text)
+                    val element = Element.BlockCode(Element.BlockCode.Type.MIDDLE, text, subs)
+                    parents.add(element)
+                    lastStartIndex = endIndex
+                }
 //
 //                //11 -> NUMERIC LIST
 //                11 -> {
